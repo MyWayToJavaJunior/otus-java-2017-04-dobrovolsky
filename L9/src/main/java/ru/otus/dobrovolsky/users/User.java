3cfeb5ccf30f4880dc36c076bd7e7adb8bfebaf7
ketaetc <ketaetc@gmail.com>
2017-06-06 21:17:58 +0500

    Had removed setters from User class

--- L9/src/main/java/ru/otus/dobrovolsky/users/User.java
+++ L9/src/main/java/ru/otus/dobrovolsky/users/User.java
@@ -41,16 +41,8 @@ public class User implements Serializable {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Integer getAge() {
         return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
     }
 
     public String toString() {
