dc79b6188161c87f7f3ad767c1ccd987efe302d4
ketaetc <ketaetc@gmail.com>
2017-07-27 01:41:03 +0500

    initial commit 1

--- L15/src/main/java/ru/otus/dobrovolsky/messageSystem/MessageSystem.java
+++ L15/src/main/java/ru/otus/dobrovolsky/messageSystem/MessageSystem.java
@@ -1,5 +1,9 @@
 package ru.otus.dobrovolsky.messageSystem;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -9,6 +13,8 @@ public final class MessageSystem {
 
     private final Map<Address, ConcurrentLinkedQueue<Message>> messagesMap = new HashMap<>();
     private final Map<Address, Addressee> addresseeMap = new HashMap<>();
+
+    private final Logger LOGGER = LoggerFactory.getLogger(MessageSystem.class);
 
     public void addAddressee(Addressee addressee) {
         addresseeMap.put(addressee.getAddress(), addressee);
@@ -26,8 +32,11 @@ public final class MessageSystem {
                 while (true) {
                     ConcurrentLinkedQueue<Message> queue = messagesMap.get(entry.getKey());
                     while (!queue.isEmpty()) {
+                        LOGGER.info("Queue size:" + queue.size());
                         Message message = queue.poll();
                         message.exec(entry.getValue());
+                        LOGGER.info(LocalDateTime.now() + ":    Message from:" + message.getFrom() + ", to:" + message
+                                .getTo());
                     }
                     try {
                         Thread.sleep(MessageSystem.DEFAULT_STEP_TIME);
